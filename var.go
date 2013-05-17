1a7ff2104de7c5b4a7a78a720454179dcaa9cbb7
palmer <mazhaoyong@gmail.com>
2013-05-17 19:01:15 +0800

    Update var.go

--- var.go
+++ var.go
@@ -1,5 +1,5 @@
 /**
- * 变量说明
+ * 变量使用章节
  */
 package main
 
@@ -7,6 +7,40 @@ import(
   "fmt"
 )
 
+//定义常量,普通定义方式
+const var1 int = 0
+
+//定义多个常量，这种方式不能用在方法内
+const(
+  var2 int = 1
+  var3 string = "定义一个变量"
+)
+
+//特殊变量 iota，初始值为0  每次赋值增加1  当遇到新的const时，再次初始化为0
+const(
+  var4 = iota//=0
+  var5        //=1
+  var6        //=2
+)
+
+//定义普通变量
+var var7 int = 1
+var var8,var9 int = 3,4
+
+var(
+  var10 int
+  var11 string
+)
+
+//最简化的定义方式
+var12 :=2.0
+var13,var14 := 5,6
+//虚数
+var c complex128 = 5+5i
+
+var a, _ = 3, 4 //特殊变量 下拉框  _ 表示接收参数，直接丢弃
+
+
 function main(){
-  fmt.Println("Hello,World!");
+  fmt.Println("var4=%d",var4);
 }
