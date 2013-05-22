b19e9f5da322c6cf003138f6b96964d3106c0c5c
palmer <mazhaoyong@gmail.com>
2013-05-22 19:42:58 +0800

    Update reg.go

--- reg.go
+++ reg.go
@@ -7,8 +7,17 @@ package main
 
 import(
   "fmt"
+  "regexp"
 )
+
+
  
 func main(){ 
+ //期望匹配的字符串
+ var url1,url2,url3 string = "/index","/book/1","/index.html"
+
+ var target1,target2 int = 100011,23456
+
+
  fmt.Println("");
 }
